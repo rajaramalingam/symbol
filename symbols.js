c3a5048a8151846563a34b99e1f0bc5759c5fc02
rajaramalingam <rajaramalingam345@gmail.com>
2017-10-02 18:54:05 +0530

    1 cmt

---
+++ symbols.js
@@ -0,0 +1,50 @@
+function myFunc1() {
+	var a = document.getElementById("num1").value;
+	var b = document.getElementById("num2").value;
+	document.getElementById("answer").innerHTML =(a<b);
+}
+function myFunc2() {
+	var a = document.getElementById("num1").value;
+	var b = document.getElementById("num2").value;
+	document.getElementById("answer").innerHTML =(a>b);
+}
+function myFunc3() {
+	var a = document.getElementById("num1").value;
+	var b = document.getElementById("num2").value;
+	document.getElementById("answer").innerHTML =(a<=b);
+}
+function myFunc4() {
+	var a = document.getElementById("num1").value;
+	var b = document.getElementById("num2").value;
+	document.getElementById("answer").innerHTML =(a>=b);
+}
+function myFunc5() {
+	var a = document.getElementById("num1").value;
+	var b = document.getElementById("num2").value;
+	document.getElementById("answer").innerHTML =(a==b);
+}
+function myFunc6() {
+	var a = document.getElementById("num1").value;
+	var b = document.getElementById("num2").value;
+	document.getElementById("answer").innerHTML =(a!==b);
+}
+function myFunc7() {
+	var a = document.getElementById("num1").value;
+	var b = document.getElementById("num2").value;
+	document.getElementById("answer").innerHTML =(a===b);
+}
+function myFunc8() {
+	var a = document.getElementById("stng1").value;
+	var b = document.getElementById("stng2").value;
+	document.getElementById("answer2").innerHTML =(a!==b);
+}
+function myFunc9() {
+	var a = document.getElementById("stng1").value;
+	var b = document.getElementById("stng2").value;
+	document.getElementById("answer2").innerHTML =(a===b);
+}
+function myFunc10() {
+	var a = document.getElementById("stng1").value;
+	var b = document.getElementById("stng2").value;
+	document.getElementById("answer2").innerHTML =(a!=b);
+}
